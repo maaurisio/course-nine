e203e656355b0801a8f0285874a7b6e14b7bf576
maaurisio <mauriciomrla@gmail.com>
2026-05-03 12:21:12 -0500

    reto 6

--- src/app/components/mi-perfil/mi-perfil.ts
+++ src/app/components/mi-perfil/mi-perfil.ts
@@ -7,8 +7,19 @@ import { Component } from '@angular/core';
   styleUrl: './mi-perfil.css',
 })
 export class MiPerfil {
+
+  public name: string;
+  public edad: string;
+  public ocupacion: string;
+  public direccion: string;
+  public telefono: number;
+
   constructor(){
-    alert('¡Bienvenido al perfil, estás dentro de la aplicación!)');
+    this.name = 'Juan Pérez';
+    this.edad = '30 años';
+    this.ocupacion = 'Desarrollador de Software';
+    this.direccion = 'Calle Falsa 123, Ciudad, País';
+    this.telefono = 1234567890;
     console.log('Se cargó el componente de perfil correctamente.');
   }
 }
--- src/app/components/product/product.ts
+++ src/app/components/product/product.ts
@@ -7,8 +7,17 @@ import { Component } from '@angular/core';
   styleUrl: './product.css',
 })
 export class Product {
+
+  public name: string;
+  public marca: string;
+  public precio: number;
+  public descripcion: string;
+
   constructor(){
+    this.name = 'PC portatil 1';
+    this.marca = 'Dell';
+    this.precio = 1000;
+    this.descripcion = 'Portátil con procesador i7 y 16GB de RAM';
     console.log('se ha cargado el componente product');
-    alert('hola');
   }
 }
