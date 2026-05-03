44c5609148c6e8411652cbb0171148f5459a17b9
maaurisio <mauriciomrla@gmail.com>
2026-05-02 21:45:03 -0500

    reto 3

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('course-nine');
+  protected readonly title = signal('aprender-angular');
 }
